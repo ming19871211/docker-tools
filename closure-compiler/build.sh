5656a635551b87bd1b5b1d9f0fe19bae7ffacfbe
梅其明 <ming19871211@163.com>
2019-04-22 22:02:03 +0800

    修改打包版本

--- closure-compiler/build.sh
+++ closure-compiler/build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-docker build -t ming19871211/closure-compiler:last .
+docker build -t ming19871211/closure-compiler:latest .
 docker build -t ming19871211/closure-compiler:20190415 .
 # 运行测试
 #docker run -d  -t --rm  --name  closure-compiler ming19871211/closure-compiler:last  cat
