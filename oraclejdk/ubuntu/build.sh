22a1be1950ec814d1ea1a3596035385b26e3c05f
梅其明 <ming19871211@163.com>
2019-08-04 16:09:39 +0800

    使用debian系统适配arm

--- oraclejdk/ubuntu/build.sh
+++ oraclejdk/ubuntu/build.sh
@@ -4,3 +4,8 @@ docker build -t ming19871211/oraclejdk:8 .
 #docker run -d --name oraclejdk ming19871211/oraclejdk:8 tail -f /dev/null
 #上传镜像
 docker push ming19871211/oraclejdk:8
+
+
+# arm64架构打包（下载oracle的arm64的jdk8包）
+# docker build -t ming19871211/oraclejdk:8-arm64 .
+# docker push ming19871211/oraclejdk:8-arm64
