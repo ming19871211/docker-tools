8926e89e525bcaf5138c36029be279d7544bba8d
梅其明 <meiqiming@talkweb.com.cn>
2020-06-04 17:42:19 +0800

    修改镜像打包

--- yapi/yapi-cli/docker-entrypoint.sh
+++ yapi/yapi-cli/docker-entrypoint.sh
@@ -4,19 +4,25 @@ set -e
 exec_cmd="$@"
 
 function exec::all_yapi_import(){
+  local count=0
+  local isYapiImport="false"
   for element in `ls $1`
   do  
-    echo
+    count=$(( $count+1 ))
     dir_or_file=$1"/"$element
     if [ -d $dir_or_file ]; then
       exec::all_yapi_import $dir_or_file
     elif [ "$element" == "yapi-import.json" ]; then
+      isYapiImport='true'
       echo "在$1目录执行数据导入"
       exec "cd $1 && $exec_cmd"
-    else
-      echo "$1目录包括没有yapi-import.json文件，不能执行导入操作哦。"
     fi  
   done
+  if [[ "$count" == 0 ]]; then
+    echo "$1目录为空目录，不能执行导入操作哦。"
+  elif [[ "$isYapiImport" == "false" ]]; then
+    echo "$1目录包括没有yapi-import.json文件，不能执行导入操作哦。"
+  fi;
 }
 # 执行/data目录下数据导入
 exec::all_yapi_import /data
