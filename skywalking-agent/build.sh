c37a289249f362ba945830ab4e0d7068d618a468
梅其明 <meiqiming@talkweb.com.cn>
2020-12-24 10:18:29 +0800

    升级skywalking到8.3.0

--- skywalking-agent/build.sh
+++ skywalking-agent/build.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-docker build -t ming19871211/skywalking-agent:7.0.0 .
+docker build -t ming19871211/skywalking-agent:8.3.0 .
 docker build -t ming19871211/skywalking-agent:latest .
 #测试
 #docker run -d --name skywalking-agent ming19871211/skywalking-agent:6.6.0  tail -f /dev/null
 #上传镜像
-docker push ming19871211/skywalking-agent:7.0.0
+docker push ming19871211/skywalking-agent:8.3.0
 docker push ming19871211/skywalking-agent:latest
